6656ed09f3c9cd1c8603499866da0702ea1fdf81
David Bradford <david.bradford@mongodb.com>
2019-09-04 10:57:25 -0400

    fix flake errors

--- src/pylibversion/__init__.py
+++ src/pylibversion/__init__.py
@@ -1,6 +1,6 @@
 """Tools to help manage versions in python libraries."""
 
-from pylibversion.version_util import (
+from pylibversion.version_util import (  # noqa: F401
     version_tuple_to_str,
     lookup_latest_version_in_pypi,
     lookup_local_module_version,
